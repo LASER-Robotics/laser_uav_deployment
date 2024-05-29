6e4638752765b2c50617855daa0f62592f1aa55b
Augusto Vinicius <augustoviniciud@gmail.com>
2024-05-29 17:15:17 -0300

    fix add user uav

--- khadas/deploy.sh
+++ khadas/deploy.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-../../miscellaneous/computer_setup/configurate_uav_constants_in_bash.sh
+~/git/laser_uav_deployment/miscellaneous/computer_setup/configurate_uav_constants_in_bash.sh
 source ~/.bashrc
-../miscellaneous/add_uav_user.sh
-../../miscellaneous/computer_setup/configurate_network.sh
-../../miscellaneous/computer_setup/disable_hibernation.sh
-../../miscellaneous/computer_setup/configurate_ssh_key.sh
+~/git/laser_uav_deployment/miscellaneous/computer_setup/configurate_network.sh
+~/git/laser_uav_deployment/miscellaneous/computer_setup/disable_hibernation.sh
+~/git/laser_uav_deployment/miscellaneous/computer_setup/configurate_ssh_key.sh
 
 cd ~/git
 git clone git@github.com:LASER-Robotics/laser_uav_system.git
 ./laser_uav_system/install.sh
 
-../miscellaneous/uart_khadas_setup.sh
+~/git/laser_uav_deployment/khadas/miscellaneous/uart_khadas_setup.sh
--- khadas/miscellaneous/add_uav_user.sh
+++ khadas/miscellaneous/add_uav_user.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+resp=""
+[[ -t 0 ]] && { read -p $'\e[1;32mWhich uav is this (please choose a name from this template: uav<number>) :\e[0m\n' resp ; }
+
 old_user=$(whoami)
 
-sudo adduser "$UAV_NAME"
-sudo usermod -aG sudo "$UAV_NAME"
-sudo groupmod -n "$UAV_NAME" "$old_user"
+sudo adduser "$resp"
+sudo usermod -aG sudo "$resp"
+sudo groupmod -n "$resp" "$old_user"
+
+cd /home/"$resp"
+sudo mkdir git
+
+sudo mv /home/"$old_user"/laser_uav_deployment /home/"$resp"/git/
