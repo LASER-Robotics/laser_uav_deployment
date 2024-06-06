31d1eb3067ed10de182da8f6bab7d3ec53965198
Augusto Vinicius <augustoviniciud@gmail.com>
2024-06-06 17:07:53 -0300

    Update add_uav_user.sh

--- khadas/miscellaneous/add_uav_user.sh
+++ khadas/miscellaneous/add_uav_user.sh
@@ -9,7 +9,6 @@ sudo adduser "$resp"
 sudo usermod -aG sudo "$resp"
 sudo groupmod -n "$resp" "$old_user"
 
-cd /home/"$resp"
-sudo mkdir git
+sudo mkdir /home/"$resp"/git
 
 sudo mv /home/"$old_user"/laser_uav_deployment /home/"$resp"/git/
