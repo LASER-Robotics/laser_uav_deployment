fa95823889a98d9077b25b1569604fbce993570a
Augusto Vinicius <augustoviniciud@gmail.com>
2024-06-12 10:17:33 -0300

    fix permission in script configurate_network.sh

--- miscellaneous/computer_setup/configurate_network.sh
+++ miscellaneous/computer_setup/configurate_network.sh
@@ -11,8 +11,8 @@ net_pass=""
 net_fix_ip=""
 [[ -t 0 ]] && { read -p $'\e[1;32mWhat is the fixed IP you want to configure? :\e[0m\n' net_fix_ip ; }
 
-nmcli device wifi connect "$net_id" password "$net_pass"
-nmcli con mod "$net_id" ipv4.addresses "$net_fix_ip"
-nmcli con mod "$net_id" ipv4.dns "8.8.8.8"
-nmcli con mod "$net_id" ipv4.method manual
+sudo nmcli device wifi connect "$net_id" password "$net_pass"
+sudo nmcli con mod "$net_id" ipv4.addresses "$net_fix_ip"
+sudo nmcli con mod "$net_id" ipv4.dns "8.8.8.8"
+sudo nmcli con mod "$net_id" ipv4.method manual
 
