5ea85c2cd5b8f530a39ac4efa22e1e336c662bdc
Augusto Vinicius <augustoviniciud@gmail.com>
2024-06-13 08:36:23 -0300

    fix bugs

--- khadas/deploy.sh
+++ khadas/deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-mkdir ~/git && cd ~/git
-sudo mv /home/khadas/laser_uav_deployment ./git/laser_uav_deployment
+mkdir ~/git
+mv ~/laser_uav_system ~/git/
 
 ~/git/laser_uav_deployment/miscellaneous/computer_setup/configurate_uav_constants_in_bash.sh
 source ~/.bashrc
+
 ~/git/laser_uav_deployment/miscellaneous/computer_setup/configurate_network.sh
 ~/git/laser_uav_deployment/miscellaneous/computer_setup/disable_hibernation.sh
 ~/git/laser_uav_deployment/miscellaneous/computer_setup/configurate_ssh_key.sh
 
+cd ~/git
 git clone git@github.com:LASER-Robotics/laser_uav_system.git
 ./laser_uav_system/install.sh
 
