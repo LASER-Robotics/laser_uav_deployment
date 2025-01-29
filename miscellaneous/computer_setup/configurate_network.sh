94d868d9e84b4077daa98bc4d212e3695c74a50e
Augusto Vinicius <augustoviniciud@gmail.com>
2025-01-29 14:03:05 -0300

    Update configurate_network.sh

--- miscellaneous/computer_setup/configurate_network.sh
+++ miscellaneous/computer_setup/configurate_network.sh
@@ -11,8 +11,12 @@ net_pass=""
 net_fix_ip=""
 [[ -t 0 ]] && { read -p $'\e[1;32mWhat is the fixed IP you want to configure? :\e[0m\n' net_fix_ip ; }
 
+net_fix_gateway=""
+[[ -t 0 ]] && { read -p $'\e[1;32mWhat is the fixed IP you want to configure? :\e[0m\n' net_fix_gateway ; }
+
 sudo nmcli device wifi connect "$net_id" password "$net_pass"
 sudo nmcli con mod "$net_id" ipv4.addresses "$net_fix_ip"
+sudo nmcli con mod "$net_id" ipv4.gateway "$net_fix_gateway"
 sudo nmcli con mod "$net_id" ipv4.dns "8.8.8.8"
 sudo nmcli con mod "$net_id" ipv4.method manual
 
