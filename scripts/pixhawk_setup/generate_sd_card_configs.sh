2b6b8c7b4882a7baa03f9b346d5a6aa7d07bd84e
Augusto-Viniciuss <augustoviniciud@gmail.com>
2025-10-24 11:05:30 -0300

    fix script that generate px4 configs into sdcard

--- scripts/pixhawk_setup/generate_sd_card_configs.sh
+++ scripts/pixhawk_setup/generate_sd_card_configs.sh
@@ -5,14 +5,14 @@ rm -r ../../miscellaneous/pixhawk/sdcard_config/etc/*
 touch ../../miscellaneous/pixhawk/sdcard_config/etc/extras.txt
 touch ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
 
-echo "uxrce_dds_client start -t serial -d /dev/ttyS3 -b 921600 -n $UAV_NAME" >> ../../miscellaneous/pixhawk/sdcard_config/etc/extras.txt
+echo "uxrce_dds_client start -t serial -d /dev/ttyS3 -b 2000000 -n $UAV_NAME" >> ../../miscellaneous/pixhawk/sdcard_config/etc/extras.txt
 
-echo "param set-default UXRCE_DDS_CFG 102" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
-echo "param set-default UXRCE_DDS_DOM_ID $ROS_DOMAIN_ID" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
-echo "param set-default SER_TEL2_BAUD 921600" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
-echo "param set-default DSHOT_TEL_CFG 103" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
-echo "param set-default MAV0_CONFIG 101" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
-echo "param set-default MAV1_CONFIG 103" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
-echo "param set-default MAV2_CONFIG 0" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
+echo "param set UXRCE_DDS_DOM_ID $ROS_DOMAIN_ID" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
+echo "param set UXRCE_DDS_KEY 1" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
+echo "param set SER_TEL2_BAUD 2000000" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
+echo "param set DSHOT_TEL_CFG 103" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
+echo "param set MAV0_CONFIG 101" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
+echo "param set MAV1_CONFIG 103" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
+echo "param set MAV2_CONFIG 0" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
 
 echo "Paste and copy etc folder into sd card, the etc folder can be find in \"/laser_uav_deployment/miscellaneous/pixhawk/sdcard_config\""
