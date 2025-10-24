42f1796f555fb3b0b3d658bc90a0534b89232c77
Augusto-Viniciuss <augustoviniciud@gmail.com>
2025-10-24 11:10:08 -0300

    fix .gitignore and changes in launch uxrce

--- launch/uxrce.launch.py
+++ launch/uxrce.launch.py
@@ -8,9 +8,14 @@ from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Text
 from launch_ros.substitutions import FindPackageShare
 
 def launch_setup(context: launch.LaunchContext, ld):
+
+    # #{ serial_port
+    serial_port = LaunchConfiguration('serial_port')
+    # #}
+
     # #{ start uxrce protocol
     uxrce_script_cmd = ExecuteProcess(
-        cmd=["sudo" ,"MicroXRCEAgent", "serial", "--dev", "/dev/pixhawk", "-b", "921600"],
+        cmd=["sudo" ,"MicroXRCEAgent", "serial", "--dev", serial_port.perform(context), "-b", "2000000"],
         name="uxrce_protocol",
         output='screen'
     )
@@ -20,6 +25,14 @@ def launch_setup(context: launch.LaunchContext, ld):
 
 def generate_launch_description():
     ld = launch.LaunchDescription()
+
+    # #{ serial_port
+    ld.add_action(DeclareLaunchArgument(
+        'serial_port',
+        default_value='/dev/pixhawk',
+        description='Serial port to connect with fly controller.'
+    ))
+    # #}
 
     # #{ opaque function
 
