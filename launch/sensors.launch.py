d25893025c2ae25a209752cccd5c2e6cf896467a
Augusto-Viniciuss <augustoviniciud@gmail.com>
2025-10-29 09:47:22 -0300

    fix launch

--- launch/sensors.launch.py
+++ launch/sensors.launch.py
@@ -61,7 +61,7 @@ def launch_setup(context: launch.LaunchContext, ld):
                 package = 'realsense2_camera'
                 launch_name = 'rs_camera.launch.py'
                 launch_arguments={
-                    'camera_name': sensor.get('name', '')
+                    'camera_name': sensor.get('name', ''),
                     'camera_params_file': sensor.get('config_file_path', PathJoinSubstitution([FindPackageShare(package), 'params', 'default.yaml']))
                 }
             if 'livox' == sensor.get('type', ""):
