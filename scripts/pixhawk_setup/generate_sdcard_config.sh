33a3b4cc2441296c468be5d00e4da463bdef7196
Augusto-Viniciuss <augustoviniciud@gmail.com>
2025-10-31 12:40:25 -0300

    change firmware to 1.16 because have support for bidirectional dshot and fix script to generate sdcard configs to include aditional configs

--- scripts/pixhawk_setup/generate_sdcard_config.sh
+++ scripts/pixhawk_setup/generate_sdcard_config.sh
@@ -10,8 +10,10 @@ echo "usleep 100000" >> ../../miscellaneous/pixhawk/sdcard_config/etc/extras.txt
 
 echo "param set UXRCE_DDS_DOM_ID $ROS_DOMAIN_ID" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
 echo "param set UXRCE_DDS_KEY 1" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
+echo "param set SER_TEL1_BAUD 0" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
 echo "param set SER_TEL2_BAUD 2000000" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
 echo "param set DSHOT_TEL_CFG 103" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
+echo "param set DSHOT_BIDIR_EN 1" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
 echo "param set MAV0_CONFIG 101" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
 echo "param set MAV1_CONFIG 103" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
 echo "param set MAV2_CONFIG 0" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
