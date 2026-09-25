a1df96553f937300be4a6f9023d89672014cc0ba
Augusto Vinicius <augustoviniciud@gmail.com>
2026-09-25 16:04:22 -0300

    fix sdcard config generator for px4 autopilot

--- scripts/pixhawk_setup/generate_sdcard_config.sh
+++ scripts/pixhawk_setup/generate_sdcard_config.sh
@@ -12,10 +12,10 @@ echo "param set UXRCE_DDS_DOM_ID $ROS_DOMAIN_ID" >> ../../miscellaneous/pixhawk/
 echo "param set UXRCE_DDS_KEY 1" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
 echo "param set SER_TEL1_BAUD 0" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
 echo "param set SER_TEL2_BAUD 2000000" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
-echo "param set DSHOT_TEL_CFG 103" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
+echo "param set DSHOT_TEL_CFG 0" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
 echo "param set DSHOT_BIDIR_EN 1" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
 echo "param set MAV_0_CONFIG 101" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
-echo "param set MAV_1_CONFIG 102" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
+echo "param set MAV_1_CONFIG 103" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
 echo "param set MAV_2_CONFIG 0" >> ../../miscellaneous/pixhawk/sdcard_config/etc/config.txt
 
 echo "Paste and copy etc folder into sd card, the etc folder can be find in \"/laser_uav_deployment/miscellaneous/pixhawk/sdcard_config\""
